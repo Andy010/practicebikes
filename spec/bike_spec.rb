0e3724ea9670bd65fd442de38d961988d2829066
Andy010 <andy_gates_010@hotmail.co.uk>
2014-11-18 17:05:46 +0000

    Created lib & spec. Created bike class & spec

---
+++ spec/bike_spec.rb
@@ -0,0 +1,11 @@
+require 'bike'
+
+describe Bike do
+
+	let(:bike) { Bike.new }
+	
+	it "should not be broken" do
+		expect(bike).to_not be_broken                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+	end
+
+end
